1d147130ae7f5ea25d0477202677ced63bccdea8
Adiel Marcelo <adielcarvalhoti@gmail.com>
2026-03-04 21:36:59 -0300

    criando funcao para verificar estado

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const input = document.querySelector("#pesquisar")
 const statusApi = document.querySelector("#statusApi");
 const botao = document.querySelector("#btnEstado")
 const resultado =document.querySelector("#resultado")
@@ -31,3 +32,16 @@ function consultarStatus (){
 consultarStatus();
 
 //Verificação de caso do covid nos estados brasileiro
+function verificarEstado(){
+    const nome = input.value.toLowerCase().trim();
+
+    if (nome === "") {
+        resultado.innerHTML = "Digite um Estado válido!";
+        return;
+    }
+  fetch("https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/sp", {
+  "method": "GET"
+})
+.then(resposta => resposta.json())
+.catch(error => console.error(error));
+}
